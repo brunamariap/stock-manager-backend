30a3edb1345c6fd807010e72572fa832288dc666
Bruna Maria <bruna.mariap@outlook.com>
2023-04-20 21:31:28 -0300

    Separando partes do sistema

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    #path('', views.products_list, name='products'),
-    #path('', views.ProductViewSet, name='products'),
-    #path('vendidos', views.products_sold_list, name='soldproducts'),
+    
 ]
