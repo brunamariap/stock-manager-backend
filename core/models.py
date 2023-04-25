03b5298ad89e9c0c57f464c87531dc04b14176d9
Bruna Maria <bruna.mariap@outlook.com>
2023-04-24 22:05:47 -0300

    relacionamento entre tabelas

--- core/models.py
+++ core/models.py
@@ -8,6 +8,9 @@ class Product(models.Model):
     created_at = models.DateTimeField(verbose_name='Data de criação', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='Data de atualização', auto_now=True)
 
+    def __str__(self):
+        return self.name
+
 
 class ProductSold(models.Model):
     product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
--- core/serializers.py
+++ core/serializers.py
@@ -2,13 +2,13 @@ from .models import Product, ProductSold
 from rest_framework import serializers
 
 
-class ProductSerializer(serializers.ModelSerializer):
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'name', 'amount', 'unitary_value', 'created_at', 'updated_at']
+        fields = ['id', 'url','name', 'amount', 'unitary_value', 'created_at', 'updated_at']
 
 
-class ProductSoldSerializer(serializers.ModelSerializer):
+class ProductSoldSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ProductSold
-        fields = ['id', 'product_id', 'sold_amount', 'final_value', 'created_at']   
+        fields = ['id','url', 'product', 'sold_amount', 'final_value', 'created_at']   
